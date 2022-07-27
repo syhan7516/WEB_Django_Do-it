f2577eed06f23207534ce26eae773c1105a42c6f
syhan7516 <syhan7516@naver.com>
2022-07-28 03:14:31 +0900

    포스트 모델 생성

--- project_abambooforest/snsapp/models.py
+++ project_abambooforest/snsapp/models.py
@@ -1,3 +1,8 @@
+from statistics import mode
 from django.db import models
 
-# Create your models here.
+# 게시물 모델
+class Post(models.Model):
+    titile = models.CharField(max_length=200)
+    body = models.TextField()
+    date = models.DateField(auto_now_add=True)
