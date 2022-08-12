7bc68017111ec72cff156207500a17c66ef89052
syhan7516 <syhan7516@naver.com>
2022-08-12 21:54:01 +0900

    url 분리 실습

--- project_do_it/urls.py
+++ project_do_it/urls.py
@@ -14,14 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 
-# Test App url 추가를 위한 import
+# 기본 urls path에서 url 분리를 위해 include 추가
+from django.urls import path
+from django.urls import include
+
+# test_basic App url 추가를 위한 import
 from test_basic import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # test App path 접근 → views파일의 test_function 리턴
+    # test_baisc App path 접근 → views파일의 test_function 리턴
     path('test_basic/',views.test_basic_function),
+
+    # test_separation App관련 모든 path 접근 → test_separation App의 urls file에서 처리
+    path('test_separation/',include('test_separation.urls'))
 ]
