5a2a2395f6eda15bd9e54dadf0e7822bd11abe87
syhan7516 <syhan7516@naver.com>
2022-08-12 20:52:00 +0900

    개발환경 셋팅 & 프로젝트 생성

--- project_abambooforest/snsapp/models.py
+++
@@ -1,8 +0,0 @@
-from statistics import mode
-from django.db import models
-
-# 게시물 모델
-class Post(models.Model):
-    titile = models.CharField(max_length=200)
-    body = models.TextField()
-    date = models.DateField(auto_now_add=True)
